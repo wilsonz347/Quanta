567fa840f38a68cca0aaf33d5badeb0895842d73
Wilson <wilsonfzheng@gmail.com>
2025-02-26 09:09:30 -0500

    load ClinicalBERT tokenizer & model

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
+from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 load_dotenv()
 
 class Config:
-    MODEL_PATH = os.getenv('MODEL_PATH', '../models/llama2_model.gguf')
+    MODEL = AutoModelForMaskedLM.from_pretrained("medicalai/ClinicalBERT")
+    TOKENIZER = AutoTokenizer.from_pretrained("medicalai/ClinicalBERT")
     TEMPERATURE = float(os.getenv('TEMPERATURE', 0.7))
     MAX_TOKENS = int(os.getenv('MAX_TOKENS', 512))
     CONTEXT_WINDOW = int(os.getenv('CONTEXT_WINDOW', 2048))
